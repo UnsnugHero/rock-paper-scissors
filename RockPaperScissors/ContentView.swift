fbd74fbc5ea82a83f55f49280f0386ca05228536
unsnugHero <unsnugHero@gmail.com>
2024-05-08 22:15:25 -0400

    feat: basic app set up

--- RockPaperScissors/ContentView.swift
+++ RockPaperScissors/ContentView.swift
@@ -8,14 +8,52 @@
 import SwiftUI
 
 struct ContentView: View {
+    private var moves = ["Scissors": "✂️", "Rock": "🪨", "Paper": "📄"]
+    
+    @State private var shouldWin = false
+    @State private var cpuChoice = ""
+    @State private var score = 0
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        // Title
+        Text("Rock, paper, scissors?").font(.title)
+        
+        // Score
+        Text("Score: \(score)")
+        
+        // CPU move
+        
+        // Win/Lose
+        // Play a winning move/Play a losing move
+        // make winning green and losing red
+        MovePrompt(isWinningMove: shouldWin)
+        
+        // Three buttons with moves in them + emojies
+        HStack {
+            ForEach(moves.keys.sorted(), id: \.self) { key in
+                Button {
+                    print("Clicked \(key)")
+                } label: {
+                    VStack {
+                        Text(moves[key] ?? "Uh oh")
+                        Text(key)
+                    }
+                    .padding()
+                    .background(.red)
+                }
+            }
         }
-        .padding()
+    }
+}
+
+struct MovePrompt: View {
+    var isWinningMove: Bool
+    var promptText: String { isWinningMove ? "winning" : "losing" }
+    
+    var body: some View {
+        Group {
+            Text("Play a ") + Text(promptText).foregroundStyle(isWinningMove ? .green : .red) + Text(" move against the above move")
+        }.font(.title2).multilineTextAlignment(.center)
     }
 }
 
